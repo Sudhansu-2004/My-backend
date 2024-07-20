d953818a230387ddefdab5ae7c2fea2473fab7ef
Sudhansu-2004 <ece.22becg59@silicon.ac.in>
2024-07-20 18:46:26 +0530

    all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser =asyncHandler(async(req,res)=>{
     //check for user creation
     //return response
     const {fullName,email,username,password}=req.body
-    console.log("email: ",email);
+    //console.log("email: ",email);
 
     if(
         [fullName,email,username,password].some((field)=>
@@ -26,7 +26,7 @@ const registerUser =asyncHandler(async(req,res)=>{
     }
 
 
-    const existedUser=User.findOne({
+    const existedUser=await User.findOne({
         $or:[{ username },{ email }]
     })
 
@@ -34,8 +34,19 @@ const registerUser =asyncHandler(async(req,res)=>{
         throw new ApiError(409,"User with email or username already exists")
     }
 
+   // console.log(req.files);
+
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
+
+
+
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
@@ -69,9 +80,6 @@ const registerUser =asyncHandler(async(req,res)=>{
     return res.status(201).json(
         new ApiResponse(200,createdUser,"User registered Successfully")
     )
-
-
-
 
 
 })
